c6403506079bf2762267efbd5928e24209dce805
Rioyw69 <yogaswarario69@gmail.com>
2020-05-09 14:24:41 +0700

    :bug: fix

--- src/global.js
+++ src/global.js
@@ -1,3 +1,9 @@
-const url = 'http://localhost/backend/public/api'
+let url
+
+if (!processs.env.NODE_ENV || process.env.NODE_ENV === 'development' ) {
+    url = 'http://localhost/backend/public/api'
+} else {
+    url = 'https://tingkat-stress.herokuapp.com/api'
+}
 
 export { url }
